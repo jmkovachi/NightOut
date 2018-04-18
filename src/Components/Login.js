79dfd391df05c9ab49da44902fed4b846c98e25c
jmkovachi <johnm.kovachi@gmail.com>
2018-04-17 18:59:10 -0700

    Add sign in component

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -13,7 +13,7 @@ import {
 export default class Login extends Component {
   constructor(props) {
     super(props);
-    state = {
+    this.state = {
       username : null,
       password : null,
       register : false,
@@ -22,37 +22,48 @@ export default class Login extends Component {
   }
 
   onClick() {
-    fetch('https://vast-fortress-13759.herokuapp.com/api/login', {
+    /*fetch('https://vast-fortress-13759.herokuapp.com/api/login', {
       method : 'POST',
       body : JSON.stringify({ username : this.state.username,
                               password : this.state.password}),
 
 
-    }
+    }*/
   }
 
   render() {
     return (
       <Container style={styles.container}>
         <View style={styles.loginBox}>
-        
-          <Item rounded style={styles.username}>
-            <Input onChangeText={((text) => this.setState({ text : text }))}
-            placeholder='Username'/>
-          </Item>
-          <Item rounded style={styles.password}>
-            <Input onChangeText={((text) => this.setState({ text : text }))}
-            placeholder='Password'/>
-          </Item>
-          <Button bordered warning style={styles.button} onPress={() => this.onClick()}>
-            <Text> Submit </Text>
-          </Button>
-
+          { this.state.register ?
+              <Text> Hi </Text>
+          :
+            <Container>
+              <Item rounded style={styles.username}>
+                <Input onChangeText={((text) => this.setState({ text : text }))}
+                placeholder='Username'/>
+              </Item>
+              <Item rounded style={styles.password}>
+                <Input secureTextEntry={true} onChangeText={((text) => this.setState({ text : text }))}
+                placeholder='Password'/>
+              </Item>
+              <Button bordered warning
+                      style={styles.button}
+                      onPress={() => this.onClick()}>
+                <Text> Submit </Text>
+              </Button>
+            </Container>
+        }
         </View>
-        <Text> {this.state.register ? "" : "Don't have an account? Sign up."} </Text>
-        <Button onPress={() => {this.state.register = !this.state.register;}}>
-          <Text> { this.state.register ? "Back to Login" : "Register" } </Text>
-        </Button>
+        <View style={styles.register}>
+          <Text > {this.state.register ? "" : "Don't have an account? Sign up."} </Text>
+          <Button bordered warning
+                onPress={() => {this.setState({
+                register : !this.state.register,
+              })}} style={styles.button}>
+            <Text> { this.state.register ? "Back to Login" : "Register" } </Text>
+          </Button>
+        </View>
       </Container>
     );
   }
@@ -85,6 +96,11 @@ const styles = StyleSheet.create({
   },
   button : {
     alignSelf : 'center',
-    margin : 30,
+    margin : 10,
+  },
+  register : {
+    position : 'absolute',
+    top : Dimensions.get('window').height - 100,
+    alignSelf : 'center',
   }
 });
